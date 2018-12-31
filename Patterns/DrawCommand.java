390476bb97c3cdec286e4dcd7a6dd2e6128b5b79
robbymister <44422175+robbymister@users.noreply.github.com>
2018-12-31 01:39:25 -0500

    Update and rename DrawCommand.java to Patterns/DrawCommand.java

--- Patterns/DrawCommand.java
+++ Patterns/DrawCommand.java
@@ -1,9 +1,7 @@
-package ca.utoronto.utm.paint;
 import javafx.scene.canvas.GraphicsContext;
 
 /**
  * Interface that allows the shapes to draw themselves
- * @author burnser2
  *
  */
 public interface DrawCommand {
